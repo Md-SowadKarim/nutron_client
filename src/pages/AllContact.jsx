d4d6211c85614dd7a77ff51fa8c914888a76c5df
Md. Sowad Karim <43478252+Md-SowadKarim@users.noreply.github.com>
2024-01-15 19:05:25 +0600

    loader
    added the loading state to prevent the broke down of live link

--- src/pages/AllContact.jsx
+++ src/pages/AllContact.jsx
@@ -5,14 +5,19 @@ import { useQuery } from '@tanstack/react-query'
 import { getAllContact } from './../api/contact';
 
 const AllContact = () => {
-  const { data: datas = [], refetch } = useQuery({
+  const { data: datas = [], refetch,isLoading } = useQuery({
     queryKey: ['contacts'],
     queryFn: async () => await getAllContact(),
   })
  //const datas=useLoaderData()
  console.log(datas)
 
-
+if(isLoading){
+  return <div className='mx-auto'>
+    <h1 className='text-9xl'>is loading</h1>
+    <span className="loading text-9xl my-8 loading-spinner loading-lg"></span>
+  </div> 
+}
   return (
     <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-3 relative'>
       {datas.map((data,idx)=><Card  key={idx} data={data} refetch={refetch}/>)}
